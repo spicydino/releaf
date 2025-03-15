5510932ddb852ef03c7c7fbaf118ae6f0951e5e4
spicydino <02adityakoparkar@gmail.com>
2025-03-15 16:47:00 +0530

    Refactor appBar formatting in HomePage widget

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -12,8 +12,8 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            
+        appBar: AppBar( 
+          
         ),
         body: Center(
           child: Column(
